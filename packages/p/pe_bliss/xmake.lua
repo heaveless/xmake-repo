f7a6e95596670a6fe75bd9a217c13c42dd2a6d55
heaveless <me@heaveless.com>
2023-11-14 02:38:54 -0500

    fix: change url package

--- packages/p/pe_bliss/xmake.lua
+++ packages/p/pe_bliss/xmake.lua
@@ -1,7 +1,7 @@
 package("pe_bliss")
   set_description("The pe_bliss package")
 
-  add_urls("https://github.com/heaveless/pe_bliss.git")
+  add_urls("https://github.com/heaveless/pe-bliss.git")
   add_versions("v1.0.0", "f386e44f6db0d2a822d784be0fc37d33e2e2383f")
 
   on_install(function (package)
