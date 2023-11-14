ed36148b85d741b1640a951ae9dd8146c68cde92
heaveless <me@heaveless.com>
2023-11-14 02:38:00 -0500

    :memo: add pe_bliss library

---
+++ packages/p/pe_bliss/xmake.lua
@@ -0,0 +1,13 @@
+package("pe_bliss")
+  set_description("The pe_bliss package")
+
+  add_urls("https://github.com/heaveless/pe_bliss.git")
+  add_versions("v1.0.0", "f386e44f6db0d2a822d784be0fc37d33e2e2383f")
+
+  on_install(function (package)
+    local configs = {}
+    if package:config("shared") then
+      configs.kind = "shared"
+    end
+    import("package.tools.xmake").install(package, configs)
+  end)
