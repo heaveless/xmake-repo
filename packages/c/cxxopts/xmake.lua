70e97b968965d1b4c3e495283ef22084ed5bd0d6
heaveless <me@heaveless.com>
2023-11-14 04:20:59 -0500

    fix: :clown_face: remove v xd

--- packages/c/cxxopts/xmake.lua
+++ packages/c/cxxopts/xmake.lua
@@ -1,7 +1,7 @@
 package("cxxopts")
   set_description("Lightweight C++ command line option parser")
 
-  add_urls("https://github.com/jarro2783/cxxopts/archive/refs/tags/v$(version).tar.gz")
+  add_urls("https://github.com/jarro2783/cxxopts/archive/refs/tags/$(version).tar.gz")
   add_versions("v13.1.1", "eb787304d67ec22f7c3a184ee8b4c481d04357fd")
 
   on_install(function (package)
