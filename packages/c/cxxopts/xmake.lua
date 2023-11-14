2c1bdcbf68492e27c9c0ca4491925fa98d93aff2
heaveless <me@heaveless.com>
2023-11-14 04:26:01 -0500

    fix: wtf github

--- packages/c/cxxopts/xmake.lua
+++ packages/c/cxxopts/xmake.lua
@@ -1,8 +1,8 @@
 package("cxxopts")
   set_description("Lightweight C++ command line option parser")
 
-  add_urls("https://github.com/jarro2783/cxxopts/archive/refs/tags/$(version).tar.gz")
-  add_versions("v13.1.1", "eb787304d67ec22f7c3a184ee8b4c481d04357fd")
+  add_urls("https://github.com/jarro2783/cxxopts.git")
+  add_versions("v3.1.1", "eb787304d67ec22f7c3a184ee8b4c481d04357fd")
 
   on_install(function (package)
     os.cp("include/*.hpp", package:installdir("include", "cxxopts"))
