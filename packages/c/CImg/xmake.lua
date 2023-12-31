5c1eae78ca947d0292d3394021561a4f636c16ee
heaveless <me@heaveless.com>
2023-12-31 14:30:06 -0500

    fix: fixed CImg

--- packages/c/CImg/xmake.lua
+++ packages/c/CImg/xmake.lua
@@ -5,5 +5,5 @@ package("CImg")
     add_versions("v1.0.0", "c56a0a14ec7ebafcb9c4ea2e6612d783ef8e81f6")
 
     on_install(function (package)
-        os.cp("Cimg.h", package:installdir("include", "CImg"))
+        os.cp("CImg.h", package:installdir("include", "CImg"))
     end)
