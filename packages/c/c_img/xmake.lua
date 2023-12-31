53a04d826d3557d68e0acc7e13843236e9fe61ce
heaveless <me@heaveless.com>
2023-12-31 14:40:18 -0500

    fix: change config

--- packages/c/c_img/xmake.lua
+++
@@ -1,9 +0,0 @@
-package("CImg")
-    set_description("The CImg Library is a small and open-source C++ toolkit for image processing")
-
-    add_urls("https://github.com/GreycLab/CImg.git")
-    add_versions("v1.0.0", "c56a0a14ec7ebafcb9c4ea2e6612d783ef8e81f6")
-
-    on_install(function (package)
-        os.cp("CImg.h", package:installdir("include", "CImg"))
-    end)
